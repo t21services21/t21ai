123d137d2ee25ffe5c2fdba0d58ea5ebc084f806
t21services21 <t21services21@gmail.com>
2025-06-29 17:30:06 +0100

    layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,12 +5,12 @@ import React from 'react';
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      head>
-  <script
-    src="<script src="//code.tidio.co/8nfvs8polg4lrqlkicel0meee201dtve.js" async></script>"
-    async
-  ></script>
-</head>
+      <head>
+        <script
+          src="https://code.tidio.co/8nfvs8polg4lrqlkicel0meee201dtve.js"
+          async
+        ></script>
+      </head>
       <body>{children}</body>
     </html>
   );
